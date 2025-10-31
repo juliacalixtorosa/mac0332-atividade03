0d96b3f35280783751f84634ab1e6dfdd39c372c
Muriel <murilatorre04@gmail.com>
2025-10-31 10:52:07 -0300

    feat: implementa a lógica de verificar anagramas
    Signed-off-by: Muriel <murilatorre04@gmail.com>

--- solucoes.py
+++ solucoes.py
@@ -1,7 +1,9 @@
-def sao_anagramas(string1, string2):
-    # TODO: implementar a lógica
-
-    pass
+def sao_anagramas(string1: str, string2: str) -> bool:
+    if len(string1) != len(string2):
+        return False
+    sorted_string1 = sorted(string1)
+    sorted_string2 = sorted(string2)
+    return sorted_string1 == sorted_string2
 
 def cifra_de_cesar(texto, deslocamento):
     # TODO: implementar a lógica
